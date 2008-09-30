356198a24ad719863ef2b3de5d367373d1a29b1e
Richard Lowe <richlowe@richlowe.net>
2008-09-29 20:03:58 -0400

    6740590 Comments check can miss junk preceding bug synopsis

--- src/legacy/scm/tooltest/tests/test_comments.py
+++ src/legacy/scm/tooltest/tests/test_comments.py
@@ -24,14 +24,12 @@
 # Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
-# ident	"%Z%%M%	%I%	%E% SMI"
-#
 
 #
 # Tests for Checks.Comments
 #
 
-import unittest, sys
+import unittest
 from cStringIO import StringIO
 
 import onbld.Checks.Comments as Comments
@@ -42,16 +40,16 @@ class TestComments(unittest.TestCase):
 		Comments.comchk(comments, check_db=check_db,
 				output=out)
 		return out.getvalue()
-	
+
 	def testBlanks(self):
 		'comchk with blank in comments'
-		self.assertEquals(self.comchk(['\n'], False),
+		self.assertEquals(self.comchk([' '], False),
 				  'WARNING: Blank line(s) in comments\n')
 
 		self.assertEquals(self.comchk([''], False),
 				  'WARNING: Blank line(s) in comments\n')
 
-		self.assertEquals(self.comchk([' \t\n'], False),
+		self.assertEquals(self.comchk(['\t'], False),
 				  'WARNING: Blank line(s) in comments\n')
 
 	def testNoSpace(self):
@@ -59,6 +57,10 @@ class TestComments(unittest.TestCase):
 		self.assertEquals(self.comchk(['1111111\tsomething is broken']),
 		    'These bugs are missing a single space following the ID:\n'
 		    '  1111111\tsomething is broken\n')
+
+                self.assertEquals(self.comchk(['1111111something is broken']),
+                     'These bugs are missing a single space following the ID:\n'
+                     '  1111111something is broken\n')
 
 	def testInvalidComment(self):
 		'comchk with malformed comment'
@@ -95,7 +97,6 @@ class TestComments(unittest.TestCase):
 			  'These IDs appear more than once in your comments:\n'
 			  '  PSARC 2006/001\n')
 
-
 	def testNonExistentCR(self):
 		"comchk with nonexistent CR in comments"
 		self.assertEquals(self.comchk(['9999999 Something broke'],
@@ -110,7 +111,6 @@ class TestComments(unittest.TestCase):
 					      check_db=True),
 		       'These bugs/ARC cases were not found in the databases:\n'
 		       '  PSARC 2012/001\n')
-		
 
 	def testBadBugSynopsis(self):
 		"comchk with bad CR synopsis in comments"
@@ -131,6 +131,50 @@ class TestComments(unittest.TestCase):
 	 "  should be: 'av1394 interface promotion'\n"
 	 "         is: 'alter ping'\n")
 
+	def testValidWithJunk(self):
+		"comchk of valid bug/synopsis with junk leading/trailing text"
+
+		self.assertEquals(self.comchk(['5051903 foobar Ypserv memory leak'],
+					      check_db=True),
+	 "These bugs/ARC case synopsis/names don't match the database entries:\n"
+	 "Synopsis/name of 5051903 is wrong:\n"
+	 "  should be: 'Ypserv memory leak'\n"
+	 "         is: 'foobar Ypserv memory leak'\n")
+
+		self.assertEquals(self.comchk(['5051903 foobarYpserv memory leak'],
+					      check_db=True),
+	 "These bugs/ARC case synopsis/names don't match the database entries:\n"
+	 "Synopsis/name of 5051903 is wrong:\n"
+	 "  should be: 'Ypserv memory leak'\n"
+	 "         is: 'foobarYpserv memory leak'\n")
+
+		self.assertEquals(self.comchk(['5051903 Ypserv memory leak bar'],
+					      check_db=True),
+	 "These bugs/ARC case synopsis/names don't match the database entries:\n"
+	 "Synopsis/name of 5051903 is wrong:\n"
+	 "  should be: 'Ypserv memory leak'\n"
+	 "         is: 'Ypserv memory leak bar'\n")
+
+		self.assertEquals(self.comchk(['5051903 Ypserv memory leakbar'],
+					      check_db=True),
+	 "These bugs/ARC case synopsis/names don't match the database entries:\n"
+	 "Synopsis/name of 5051903 is wrong:\n"
+	 "  should be: 'Ypserv memory leak'\n"
+	 "         is: 'Ypserv memory leakbar'\n")
+
+		self.assertEquals(self.comchk(['6739108 Updated P2 kernel/io-multipath panic: (path_instance == mdi_pi_get_path_instance(vpkt-)vpkt_path)), file: ../../common/io/scsi/adap'],
+					      check_db=True),
+	 "These bugs/ARC case synopsis/names don't match the database entries:\n"
+	 "Synopsis/name of 6739108 is wrong:\n"
+	 "  should be: 'panic: (path_instance == mdi_pi_get_path_instance(vpkt-)vpkt_path)), file: ../../common/io/scsi/adap'\n"
+	 "         is: 'Updated P2 kernel/io-multipath panic: (path_instance == mdi_pi_get_path_instance(vpkt-)vpkt_path)), file: ../../common/io/scsi/adap'\n")
+
+	def testNewline(self):
+		"comchk with newlines"
+
+		self.assertRaises(ValueError, self.comchk, ['foo\nbar'])
+		self.assertRaises(ValueError, self.comchk, ['\nfoobar'])
+		self.assertRaises(ValueError, self.comchk, ['foobar\n'])
 
 	def testValid(self):
 		"comchk with valid comments"
@@ -162,8 +206,8 @@ class TestComments(unittest.TestCase):
 					check_db=True))
 
 		self.failIf(self.comchk(["PSARC/2007/394 ls(1) new command line options '-/' and '-%': CIFS system attributes support"],
-					    check_db=True))
-		
+					check_db=True))
+
 		self.failIf(self.comchk(['PSARC/2007/499 Automatic discovery of network attached printers'],
 					check_db=True))
 
